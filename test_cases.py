a548a6356103f34935ec6bd350714e1dc694bfb8
Irinyi Classroom User <somebody@pc22117-9.inf.u-szeged.hu>
2026-04-24 14:38:27 +0200

    dd

--- test_cases.py
+++ test_cases.py
@@ -6,3 +6,6 @@ class TestGreet(unittest.TestCase):
     self.assertEqual(greet("Alice"), "Hello, Alice")
     self.assertEqual(greet(""), "Hello, Stranger")
     self.assertEqual(greet(None), "Hello, Stranger")
+
+if __name__ == "__main__":
+ unittest.main()
